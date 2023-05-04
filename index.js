3dd753a880e236aaabc556af55ce9e0a6fe649af
nadyathompson <127708727+nadyathompson@users.noreply.github.com>
2023-05-04 01:27:46 -0400

    updated CORS

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ const passport = require('passport');
 const { check, validationResult } = require('express-validator');
 const cors = require('cors');
     app.use(cors());
-    /*let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+    /*let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];*/
 
     app.use(cors({
     origin: (origin, callback) => {
         if(!origin) return callback(null, true);
         if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-        return callback(new Error(message ), false);
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message ), false);
         }
         return callback(null, true);
     }
-    }));*/
+    }));
     let auth = require('./auth')(app);
     require("./passport");
 
@@ -34,7 +34,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //add user
 app.post('/users', 
