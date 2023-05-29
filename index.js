c513d82dfa8bec7ac5a8b2345562813f543e1c6c
nadyathompson <127708727+nadyathompson@users.noreply.github.com>
2023-05-29 14:15:59 -0400

    updated CORS to allow all origins

--- index.js
+++ index.js
@@ -13,18 +13,10 @@ const passport = require('passport');
 
 const { check, validationResult } = require('express-validator');
 const cors = require('cors');
-    app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-            return callback(new Error(message ), false);
-        }
-        return callback(null, true);
-    }
-    }));
-    let auth = require('./auth')(app);
-    require("./passport");
+app.use(cors());
+
+let auth = require('./auth')(app);
+require("./passport");
 
 app.use(morgan('common'));
 app.use(express.static('public'));
